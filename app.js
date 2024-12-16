5ef13a20bfc15fed0dcdbd2658d6f2c5c96f4078
ahmedraza97 <razauser2015@gmail.com>
2024-12-16 21:45:22 +0500

    test

--- app.js
+++ app.js
@@ -6,3 +6,48 @@ var swiper = new Swiper(".reviewSwiper", {
         clickable: true,
     },
 });
+
+
+
+var swiper = new Swiper(".categorySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".productSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".tips_Trends_Swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+var swiper = new Swiper(".reelSwipper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+
+
+
+
